504df296bae67c52f5de3710b0882ee7403c5987
Mariano San Juan <s.jmariano@gmail.com>
2022-05-03 10:26:05 -0300

    apilca react

---
+++ calendario-react/src/components/CargarLogo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from "../assets/images/logo.png"
+
+
+
+const CargarLogo = () => {
+
+  return ( 
+  
+    <div>
+      <img src={logo} alt="" className='logoInicio'/>
+      
+    </div>
+  
+  
+    
+     );
+}
+ 
+export default CargarLogo;
